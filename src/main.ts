60058b75ebeab1047cdaf79badd3f274e7c533a2
Wong Teck Han <7806684+teckhan@users.noreply.github.com>
2024-02-28 03:14:12 +0800

    feat: change logo

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 
+fetch("https://checkip.amazonaws.com/", { mode: "no-cors" })
+    .then((res) => {
+        console.log(res);
+        return res.text();
+    })
+    .then((data) => console.log("aws", data))
+    .catch((error) => console.error("Error:", error));
+
+// TODO: condition here to load client vs host
 createApp(App).mount("#app");
